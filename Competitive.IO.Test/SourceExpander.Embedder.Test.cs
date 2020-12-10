fb8b8178477121ae67577531e08dc27b4d038acc
naminodarie <gengesa@gmail.com>
2020-12-11 01:57:34 +0900

    Embedded test

---
+++ Competitive.IO.Test/SourceExpander.Embedder.Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+
+namespace Kzrnm.Competitive.IO
+{
+    public class SourceExpanderTest
+    {
+        [Fact]
+        public void Embedded()
+        {
+            var metadataDic = typeof(ConsoleReader).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+                .ToDictionary(attr => attr.Key, attr => attr.Value);
+
+            metadataDic.Should().NotContainKey("SourceExpander.EmbeddedAllowUnsafe");
+            metadataDic.Should().ContainKey("SourceExpander.EmbedderVersion");
+            metadataDic.Keys.Should().ContainSingle(key => key.StartsWith("SourceExpander.EmbeddedSourceCode"));
+
+        }
+    }
+}
