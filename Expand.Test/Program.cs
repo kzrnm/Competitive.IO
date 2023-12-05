6cd059d986b330e8c914089cd1327b4489fadae5
kzrnm <gengesa@gmail.com>
2023-12-06 03:35:00 +0900

    Expand.Test

---
+++ Expand.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SourceExpander.Testing
+{
+    using Kzrnm.Competitive.IO;
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(new ConsoleReader().Int());
+        }
+    }
+}
